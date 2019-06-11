df4a5ed42668ff083cc67dc08238d523ddf01356
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-11 14:24:15 -0500

    fixed issues for graders

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 'use strict';
 
-function getDogs(dogNumber, dogBreed) {
-  if (dogBreed) {
-    fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random/${dogNumber}`)
-      .then(response => response.json())
-      .then(jsonData => {
-        extractData(jsonData);
-        console.log(jsonData);
-      })
-      .catch(error => {
-        console.log(error);
-        alert("Breed not found");
-      }
+function getDogNumber(dogNumber) {
 
-      );
-  }
-  else {
-    fetch(`https://dog.ceo/api/breeds/image/random/${dogNumber}`)
-      .then(response=> response.json())
-      .then(jsonData => {
-        extractData(jsonData);
-        console.log(jsonData);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+  fetch(`https://dog.ceo/api/breeds/image/random/${dogNumber}`)
+    .then(response=> response.json())
+    .then(jsonData => {
+      extractData(jsonData);
+      console.log(jsonData);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
-  //response will be an object
-  //message property is an array of links
-  //loop through that array and print values
+function getDogBreed(dogBreed) {
+  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData);
+      extractData(jsonData);
+    })
+    .catch(error => {
+      console.log(error);
+      alert('Breed not found');
+    });
 }
 
 function extractData(jsonData){
-  jsonData.message.forEach(dogs=>{
-    console.log(dogs);
-    $('.dogsList').append(createTemplate(dogs));
-  });
+  if (typeof jsonData.message === 'object') {
+    jsonData.message.forEach(dogs=>{
+      console.log(dogs);
+      $('.dogsList').append(createTemplate(dogs));
+    });
+  }  
+  else {
+    $('.dogsList').append(createTemplate(jsonData.message));
+  }
 }
 
 
-function handleSubmit() {
+function handleNumberSubmit() {
   $('#js-number-select').submit(function(event) {
     event.preventDefault();
+    $('.dogsList').empty();
     const dogNumber = $('.js-number-input').val();
-    const dogBreed = $('.js-breed-input').val();
-    getDogs(dogNumber, dogBreed);
+    getDogNumber(dogNumber);
+  });
+}
 
+function handleBreedSubmit() {
+  $('#js-breed-select').submit(function(event) {
+    event.preventDefault();
+    $('.dogsList').empty();
+    const dogBreed = $('.js-breed-input').val();
+    $('.js-breed-input').val('');
+    console.log(dogBreed);
+    getDogBreed(dogBreed);
   });
 }
 
@@ -60,4 +69,9 @@ function createTemplate(dogLink){
   return template;
 }
 
-handleSubmit();
+function main(){
+  handleBreedSubmit();
+  handleNumberSubmit();
+}
+
+$(main());
